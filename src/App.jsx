4bc05e4759d06d71428b71ddfa2ae3b7b50fdeec
akmalhossain <akmalforu@gmail.com>
2024-10-17 12:05:09 +0600

    props, app(), style, {} []

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,72 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <Device name="Laptop" price="6000"></Device>
+      <Device name= "mobile" price ="8000"></Device>
+      <Device name= "watch" price ="4000"></Device>
+      <Device ></Device>
+      <Person></Person>
+      <Student grade="4" score ="700"></Student>
+      <Student grade="2" score ="800"></Student>
+      <Student></Student>
+      <Developer></Developer> */}
+    </>
+  )
+
+
+}
+
+function Person() {
+  const age = 25;
+  const money = 20;
+  const person = { name: 'sakib', age: 12 }
+  // return <h3>I am {person.name} a person age: {age+ money}</h3>
+  return <h3>I am {person.name} a person age: {person.age}</h3>
+}
+function Student({grade=1, score=0}) { 
+  return (
+    <div className='student'>
+      <h3>This is a student</h3>
+      <p>Class: {grade}</p>
+      <p>score: {score}</p>
+
+    </div>
+  )
+}
+
+function Developer(){
+  const devStyle= {
+    margin:'20px',
+    padding: '20px',
+    border: '2px solid purple',
+    borderRadius: '20px'
+
+  }
+  return(
+    <>
+    <div style={devStyle}>
+      <h5>Developer</h5>
+      <p>Coding: </p>
+    </div>
+    <div style={{
+    margin:'20px',
+    padding: '20px',
+    border: '2px solid purple',
+    borderRadius: '20px'
+
+  }}>
+      <h5>Developer </h5>
+      <p>Coding: </p>
+    </div>
     </>
   )
 }
+function Device({name, price}){
+  // const {name, price}=props;
+  return <h2>Device Name: {name}/ price {price} </h2>
+}
+
+
 
 export default App
